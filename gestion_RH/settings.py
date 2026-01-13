c439bdd5f9756b678671d499a5feaf28a701c973
Adrien Formoso <adrien.formoso@live.fr>
2026-01-13 18:18:39 +0100

    swagger

--- gestion_RH/settings.py
+++ gestion_RH/settings.py
@@ -37,7 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
+    "app" 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+# Optionnel : Configuration du titre et de la version
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Mon API Django',
+    'DESCRIPTION': 'Documentation interactive de mon projet',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
